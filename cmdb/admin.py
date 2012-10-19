bb32e92327a923c6bcfc2d05c9448fc2bcb652e9
Sven Hergenhahn <svenxy@gmx.net>
2012-10-19 08:56:48 +0200

    initial commit with django structure and two initial models

---
+++ cmdb/admin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Name			:	
+# Description	:	
+# Author		: Sven Hergenhahn
+#
+# $Id$
+# 
+###################################################
+
+from cmdb.models import CiHardware
+from django.contrib import admin
+
+admin.site.register(CiHardware)
+
---
+++ cmdb/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class CiBase(models.Model):
+    type     = models.CharField(max_length=30)
+    name     = models.CharField(max_length=40)
+    supplier = models.CharField(max_length=40)
+    status   = models.CharField(max_length=40)
+    comment  = models.CharField(max_length=200)
+    is_template = models.BooleanField()
+    create_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+      abstract = True
+
+class CiHardware(CiBase):
+    model   = models.ForeignKey('self')
+    dimensions  = models.CharField(max_length=40)
+    usage       = models.CharField(max_length=80)
+
+#class Choice(models.Model):
+#    poll = models.ForeignKey(Poll)
+#    choice = models.CharField(max_length=200)
+#    votes = models.IntegerField()
